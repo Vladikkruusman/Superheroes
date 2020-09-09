df2cadb3e5da174a2ee50e53737dabb3eb32ed29
Opilane TTHK <opilane@tthk.ee>
2020-09-09 10:53:35 +0300

    supervillians

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 let superheroes = require("superheroes");
-console.log(superheroes.random());
+let supervillains = require("supervillains")
 
 superheroes.all.forEach(hero => {
     console.log(hero);
 });
+supervillains.all.forEach(villain => {
+    console.log(villain);
+})
+
+console.log(superheroes.random());
+console.log(supervillains.random());
+
+console.log(`${superheroes.random()} fights ${supervillains.random()}`);
 
 //console.log(superheroes.all);
